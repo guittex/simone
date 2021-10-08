5d0ad17a34b609ba86e2b390ebed31d3fd850dcb
guittex <ilhermeoliveira@hotmail.com>
2021-10-07 23:11:30 -0300

    Iniciando projeto

---
+++ templates/element/menu.php
@@ -0,0 +1,15 @@
+<ul class="sidebar-menu" data-widget="tree">
+    <li class="treeview">
+        <a href="#">
+            <i class="fa fa-building"></i> <span>Administrativo</span>
+            <span class="pull-right-container">
+                <i class="fa fa-angle-left pull-right"></i>
+            </span>
+        </a>
+        <ul class="treeview-menu">
+            <li><a href="<?php echo $this->Url->build('/'); ?>"><i class="fa fa-user"></i>Clientes</a></li>
+            <li><a href="<?php echo $this->Url->build('/pages/home2'); ?>"><i class="fa fa-car"></i> Carros</a></li>
+        </ul>
+    </li> 
+    
+</ul>
---
+++ templates/element/user-panel.php
@@ -0,0 +1,8 @@
+<div class="user-panel">
+    <div class="pull-left image">
+        <?php echo $this->Html->image('user2-160x160.jpg', array('class' => 'img-circle', 'alt' => 'User Image')); ?>
+    </div>
+    <div class="pull-left info">
+        <p style="margin-top:15px">Administrador</p>
+    </div>
+</div>
